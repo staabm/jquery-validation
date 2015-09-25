staabm/jquery-validation

commit b2df81ccb6276a6a39661ebd1ddd4a54c182217e

Core: escape errorID only for use in the regex
.. not for building `aria-describedby`

src/core.js
@@ -796,11 +796,11 @@ $.extend( $.validator, {
 					// If the element is not a child of an associated label, then it's necessary
 					// to explicitly apply aria-describedby
 
-					errorID = error.attr( "id" ).replace( /(:|\.|\[|\]|\$)/g, "\\$1");
+					errorID = error.attr( "id" );
 					// Respect existing non-error aria-describedby
 					if ( !describedBy ) {
 						describedBy = errorID;
-					} else if ( !describedBy.match( new RegExp( "\\b" + errorID + "\\b" ) ) ) {
+					} else if ( !describedBy.match( new RegExp( "\\b" + (errorID.replace( /(:|\.|\[|\]|\$)/g, "\\$1")) + "\\b" ) ) ) {
 						// Add to end of list if not already present
 						describedBy += " " + errorID;
 					}

commit e84ddd460d97444392110d12b9048a0391671b22

Core: fixed validation with elements containing $ in its name
regressed in b2df81ccb6276a6a39661ebd1ddd4a54c182217e

src/core.js
@@ -799,10 +799,10 @@ $.extend( $.validator, {
 					errorID = error.attr( "id" );
 					// Respect existing non-error aria-describedby
 					if ( !describedBy ) {
-						describedBy = errorID;
+						describedBy = errorID.replace( /(\[|\]|\$)/g, "\\$1");
 					} else if ( !describedBy.match( new RegExp( "\\b" + (errorID.replace( /(:|\.|\[|\]|\$)/g, "\\$1")) + "\\b" ) ) ) {
 						// Add to end of list if not already present
-						describedBy += " " + errorID;
+						describedBy += " " + (errorID.replace( /(\$)/g, "\\$1"));
 					}
 					$( element ).attr( "aria-describedby", describedBy );
 
@@ -812,7 +812,7 @@ $.extend( $.validator, {
 						$.each( this.groups, function( name, testgroup ) {
 							if ( testgroup === group ) {
 								$( "[name='" + name + "']", this.currentForm )
-									.attr( "aria-describedby", error.attr( "id" ) );
+									.attr( "aria-describedby", errorID );
 							}
 						});
 					}